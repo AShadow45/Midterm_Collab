ed76f59dad432f7d0348692119ffd7d01b15c7b8
AShadow45 <guada271@newschool.edu>
2020-02-15 13:53:24 -0500

    Edited Title scene. Start Button will go to scene 4F. Quit button coded.

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    public void StartB()
+    {
+        SceneManager.LoadScene("4F");
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();   
+    }
+}
