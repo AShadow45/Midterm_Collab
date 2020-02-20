b3165f81388f0ed50aaa40746ab6972bd7b384cd
AShadow45 <guada271@newschool.edu>
2020-02-20 13:59:54 -0500

    Enemy Sprite Code Edits

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -10,9 +10,6 @@ public class EnemyPatrol : MonoBehaviour
     //..............................................DIRECTION
     int xDirect = -1;
     int yDirect = -1;
-    //..............................................SCALE
-    float xScale;
-    float yScale;
     //..............................................POSITION
     float yStartPos;
     float yPos = 0;
@@ -31,7 +28,6 @@ public class EnemyPatrol : MonoBehaviour
     void Start()
     {
         yStartPos = transform.position.y;
-        xScale = transform.localScale.x;
     }
     
     void Update()
@@ -39,25 +35,21 @@ public class EnemyPatrol : MonoBehaviour
         if (transform.position.x < xnegat)
         {
             xDirect = 1;
-            transform.localScale = new Vector2(-xScale, transform.localScale.y);
         }
 
         if (transform.position.x > xposit)
         {
             xDirect = -1;
-            transform.localScale = new Vector2(xScale, transform.localScale.y);
         }
 
         if (transform.position.y < ynegat)
         {
             yDirect = 1;
-            transform.localScale = new Vector2(transform.localScale.x, -yScale);
         }
 
         if (transform.position.y > yposit)
         {
             yDirect = -1;
-            transform.localScale = new Vector2(transform.localScale.x, yScale);
         }
 
         Vector2 newposition = transform.position;
