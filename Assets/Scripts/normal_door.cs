0962acbb2e9f8b5477df4fd3bfaa1c5408d9017f
jib77907 <leekp733@newschool.edu>
2020-02-24 14:04:14 -0500

    door script

--- Assets/Scripts/normal_door.cs
+++ Assets/Scripts/normal_door.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class normal_door : MonoBehaviour
 {
 
+    private SpriteRenderer rend;
+
     void Start()
     {
-        
+        rend = GetComponent<SpriteRenderer>();
     }
 
     
@@ -16,11 +18,23 @@ public class normal_door : MonoBehaviour
         
     }
 
+    /*
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player is here");
+
+            this.gameObject.SetActive(false);
+        }
+    }
+    */
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            rend.enabled = !rend.enabled;
         }
     }
 }
