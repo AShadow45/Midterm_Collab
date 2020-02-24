b782c74de26abd168782b8e25080b4adfdc71584
jib77907 <leekp733@newschool.edu>
2020-02-24 13:48:13 -0500

    ??

---
+++ Assets/Scripts/normal_door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class normal_door : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player is here");
+        }
+    }
+}
