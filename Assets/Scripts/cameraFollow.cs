67a9c0887e5fd36e34b5e891cf55fbf8a0b4cfa7
jib77907 <leekp733@newschool.edu>
2020-02-24 13:34:20 -0500

    Sprites & scene edits

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -7,12 +7,13 @@ public class cameraFollow : MonoBehaviour
     public Transform player;
     public Vector3 offset;
 
-
     void Update()
     {
         if (player != null)
         {
             transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
         }
+
+
     }
 }
