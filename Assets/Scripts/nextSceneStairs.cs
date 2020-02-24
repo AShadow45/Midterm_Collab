0cc76a19138cc33fa8e68fe349d3cc9bd52ec073
jib77907 <leekp733@newschool.edu>
2020-02-23 20:53:14 -0500

    scene connected from 3f --> 4f

---
+++ Assets/Scripts/nextSceneStairs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextSceneStairs : MonoBehaviour
+{
+
+    //change scene once at stairs
+
+    public string sceneName;
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
