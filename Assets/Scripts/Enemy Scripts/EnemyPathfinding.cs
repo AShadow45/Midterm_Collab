b45ed50c05552fb44a7c8a53cd17e8c873814c17
n1tz <cole.masaki@gmail.com>
2020-02-27 14:29:43 -0500

    Pathfinding

---
+++ Assets/Scripts/Enemy Scripts/EnemyPathfinding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        //set AI path to player
+        aiPath.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+    }
+}
