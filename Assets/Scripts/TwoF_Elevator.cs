b7c30eca2cc5efe5a05cc25f6d83e8e1c2e36b04
jib77907 <leekp733@newschool.edu>
2020-02-27 13:32:34 -0500

    Generator & elevator coded

---
+++ Assets/Scripts/TwoF_Elevator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TwoF_Elevator : MonoBehaviour
+{
+    public GameObject generator;
+    public string sceneName;
+    Collider2D elevatorCol;
+
+    void Start()
+    {
+        elevatorCol = GetComponent<BoxCollider2D>();
+    }
+
+   void Update()
+    {
+        if (generator.GetComponent<generator>().generatorOn == true)
+        {
+            elevatorCol.isTrigger = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+        
+                SceneManager.LoadScene(sceneName);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/generator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator : MonoBehaviour
+{
+    public bool generatorOn;
+  
+
+    void Start()
+    {
+        generatorOn = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            generatorOn = true;
+        
+        }
+    }
+}
