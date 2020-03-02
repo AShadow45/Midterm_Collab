be54f23fc8a77ad043b2f2d52f26c58749c1be5c
jib77907 <leekp733@newschool.edu>
2020-03-02 13:52:22 -0500

    tested bat

--- Assets/Scripts/VJPlayerMovement.cs
+++ Assets/Scripts/VJPlayerMovement.cs
@@ -24,6 +24,7 @@ public class VJPlayerMovement : MonoBehaviour
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0f);//to restric movement of player
         }
 
+        /*
         for (int i = 0; i < Input.touchCount; i++)
         {
             Touch thisTouch = Input.GetTouch(i);
@@ -35,6 +36,7 @@ public class VJPlayerMovement : MonoBehaviour
                 bat.SetActive(true);
             }
         }
+        */
     }
 
     void Start()
@@ -42,11 +44,11 @@ public class VJPlayerMovement : MonoBehaviour
 
         //Initialization of boundaries
         xMax = Screen.width - 0; // I used 50 because the size of player is 100*100
-        xMin = -5;
+        xMin = -500;
         yMax = Screen.height - 0;
-        yMin = -5;
+        yMin = -500;
 
-        bat.SetActive(false);
+        //bat.SetActive(false);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
