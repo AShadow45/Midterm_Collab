219366d80cab9d3dda816843f8dc810eb6e76418
AShadow45 <guada271@newschool.edu>
2020-03-02 10:51:59 -0500

    .

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public int damage;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
