e89404295f76a589303ca0aae737273f57d575bc
AShadow45 <guada271@newschool.edu>
2020-03-02 13:37:04 -0500

    Enemy Health Code

---
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("Health Settings")]
+    public int ENEM_curHealth;
+    public int ENEM_maxHealth;
+
+    void Start()
+    {
+        ENEM_curHealth = ENEM_maxHealth;
+    }
+    
+    void Update()
+    {
+        if (ENEM_curHealth > ENEM_maxHealth)
+        {
+            ENEM_curHealth = ENEM_maxHealth;
+        }
+
+        if (ENEM_curHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Bat")
+        {
+            ENEM_curHealth -= 1;
+        }
+    }
+}
