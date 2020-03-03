39c55de612d5a461494095fbeaaf4586f30d6d6b
n1tz <cole.masaki@gmail.com>
2020-03-03 13:10:36 -0500

    movementfix

--- Assets/Scripts/VJPlayerMovement.cs
+++ Assets/Scripts/VJPlayerMovement.cs
@@ -7,6 +7,8 @@ public class VJPlayerMovement : MonoBehaviour
     public float moveSpeed;
     public VJHandler jsMovement;
 
+    public Rigidbody2D rb;
+
     private Vector3 direction;
     private float xMin, xMax, yMin, yMax;
 
@@ -19,9 +21,9 @@ public class VJPlayerMovement : MonoBehaviour
 
         if (direction.magnitude != 0)
         {
-
-            transform.position += direction * moveSpeed * Time.deltaTime;
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0f);//to restric movement of player
+            rb.AddForce(direction *moveSpeed);
+           // transform.position += direction * moveSpeed * Time.deltaTime;
+            //transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0f);//to restric movement of player
         }
 
         /*
@@ -41,12 +43,13 @@ public class VJPlayerMovement : MonoBehaviour
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
 
         //Initialization of boundaries
-        xMax = Screen.width - 0; // I used 50 because the size of player is 100*100
-        xMin = -500;
-        yMax = Screen.height - 0;
-        yMin = -500;
+       // xMax = Screen.width - 0; // I used 50 because the size of player is 100*100
+       // xMin = -500;
+       // yMax = Screen.height - 0;
+        //yMin = -500;
 
         //bat.SetActive(false);
     }
