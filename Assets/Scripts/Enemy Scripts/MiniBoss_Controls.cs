39e6918a35bff5374198baaa102270752c79c3ff
jib77907 <leekp733@newschool.edu>
2020-03-09 12:07:02 -0400

    3f

--- Assets/Scripts/Enemy Scripts/MiniBoss_Controls.cs
+++ Assets/Scripts/Enemy Scripts/MiniBoss_Controls.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MiniBoss_Controls : MonoBehaviour
 {
     Rigidbody2D rb;
-  
+
     //.....................................................ATTACK
     [Header("Attack Settings")]
     public GameObject Webs;
@@ -17,7 +17,7 @@ public class MiniBoss_Controls : MonoBehaviour
     [Tooltip("Mini Boss Current Health")]
     public int mb_curHealth;
     [Tooltip("Mini Boss Maximum Health")]
-    public int mb_maxHealth = 10; 
+    public int mb_maxHealth = 10;
 
     //.....................................................STAIRWAY 
     [Header("Spider Web")]
@@ -34,62 +34,65 @@ public class MiniBoss_Controls : MonoBehaviour
         mb_curHealth = mb_maxHealth;
 
         gun.SetActive(false);
-        
+
 
         SpawnPoint = GameObject.FindGameObjectWithTag("MiniBoss").transform;
         InvokeRepeating("Damage", 0, .1f);
+    }
 
-    
-    void Update()
-    {
-        if (mb_curHealth <= 0)
+        void Update()
         {
-            StartCoroutine(FadeTo(0.0f, 1.5f));
-            webCollider.enabled = false;
-            StartCoroutine(Die());
+            if (mb_curHealth <= 0)
+            {
+                StartCoroutine(FadeTo(0.0f, 1.5f));
+                webCollider.enabled = false;
+                StartCoroutine(Die());
 
-            gun.SetActive(true);
-        
+                gun.SetActive(true);
 
+
+            }
+            //Damage();
         }
-        //Damage();
-    }
 
-    //......................................................ATTACK
+        //......................................................ATTACK
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "batHB")
+        void OnTriggerEnter2D(Collider2D col)
         {
-            mb_curHealth -= 1;
+            if (col.gameObject.tag == "batHB")
+            {
+                mb_curHealth -= 1;
+            }
         }
-    }
 
-    void Damage() {
-        for (int i = 0; i < webMax; i++) {
-            Instantiate(Webs, SpawnPoint.position, transform.rotation);
-        }
-        
-    }
-
-    //spider web fade
-    IEnumerator FadeTo(float aValue, float aTime)
-    {
-        float alpha = spiderWeb.GetComponent<Renderer>().material.color.a;
-        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        void Damage()
         {
-            Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
-            spiderWeb.GetComponent<Renderer>().material.color = newColor;
-            aud.Play();
-            yield return null;
+            for (int i = 0; i < webMax; i++)
+            {
+                Instantiate(Webs, SpawnPoint.position, transform.rotation);
+            }
+
+        }
+
+        //spider web fade
+        IEnumerator FadeTo(float aValue, float aTime)
+        {
+            float alpha = spiderWeb.GetComponent<Renderer>().material.color.a;
+            for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+            {
+                Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
+                spiderWeb.GetComponent<Renderer>().material.color = newColor;
+                aud.Play();
+                yield return null;
+            }
+        }
+
+        //destroy spider boss
+        IEnumerator Die()
+        {
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            yield return new WaitForSeconds(1.55f);
+            Destroy(this.gameObject);
         }
     }
 
-    //destroy spider boss
-    IEnumerator Die()
-    {
-        gameObject.GetComponent<Collider2D>().enabled = false;
-        yield return new WaitForSeconds(1.55f);
-        Destroy(this.gameObject);
-    }
-}
